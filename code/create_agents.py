86afea2f42428f0cb0be333ccb4d09708add7a1a
William Bailkoski <wab199@student.bham.ac.uk>
2024-01-30 16:15:08 +0000

    refactored and safely removed some unnecessary clutter

--- code/create_agents.py
+++ code/create_agents.py
@@ -1,7 +1,4 @@
-from adjacency import convert_adj_to_power_graph
-from agent import Agent, IndependentQLearning
-from ucb_marl_agent import MARL_Comm
-
+from agent import Agent, IndependentQLearning, MARL_Comm
 from hyperparameters import graph_hyperparameters, AgentType
 
 
@@ -30,6 +27,59 @@ def create_agents(num_of_agents, agent_type, num_of_episodes=0, length_of_episod
     else:
         return {f'agent_{i}': Agent(f'agent_{i}') for i in range(num_of_agents)}
 
+
+def convert_adj_to_power_graph(adj_table, gamma_hop, connection_slow=False):
+    """Converts an adjacency table for a graph into an adjacency table for the corresponding power graph of distance
+    gamma_hop
+
+    Parameters
+    -------------
+
+    adj_table - The adjacency graph to convert
+
+    gamma_hop - the amount the graph is allowed to change by
+
+    connection_slow - False means the amount of jumps is factored in (so if the node is 2 jumps away its set to 2)
+
+    Returns
+    --------
+    adjacency table of the power graph"""
+
+    # If Gamma hop = 0 there is no communication. So [[0,0], [0,0]] for 2 agents
+    if gamma_hop == 0:
+        for i in range(len(adj_table)):
+            for j in range(len(adj_table[i])):
+                adj_table[i][j] = 0
+        return adj_table
+
+    # Goes through adj_table and adds to own dictionary the neighbours for each node
+    neighbours = {z: [] for z in range(len(adj_table))}
+    for i, row in enumerate(adj_table):
+        for j, col in enumerate(row):
+            if col != 0:
+                i_neighbours = neighbours[i]
+                i_neighbours.append((j, col))
+
+    # Updates the adjacency table to be power graph.
+    for k in range(gamma_hop - 1):
+        new_table = deepcopy(adj_table)
+
+        # Goes over each agent
+        for i, row in enumerate(adj_table):
+            # Goes for every distance for node i to node j
+            for j, col in enumerate(row):
+                if col != 0:
+                    neighbours_of_j = neighbours[j]
+                    for neighbour, dis in neighbours_of_j:
+                        if adj_table[i][neighbour] == 0 and i != neighbour:
+                            if connection_slow:
+                                speed_of_others = adj_table[i][j]
+                                new_table[i][neighbour] = dis + speed_of_others
+                            else:
+                                new_table[i][neighbour] = 1
+        adj_table = new_table
+
+    return adj_table
 
 def create_marl_agents(num_of_agents, num_of_episodes, length_of_episode, gamma_hop, adjacency_table, connection_slow):
     """
--- code/use_custom.py
+++ code/use_custom.py
@@ -1,5 +1,4 @@
-"""This will create the necessary environment for use of the rest of the program.  Plug & play with this
-abstracting which environment is used"""
+"""This is a simulation of agents acting in a decentralised framework with no message passing"""
 from pettingZoo.PettingZoo.pettingzoo.mpe.custom_env.custom_env import env, parallel_env
 from hyperparameters import train_hyperparameters, agent_hyperparameters, AgentType
 from agent import IndependentQLearning
@@ -9,7 +8,6 @@ import time
 from reward_functions import final_reward
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def _policy(agent_name, agents, observation, done, time_step, episode_num):
@@ -66,36 +64,14 @@ def _policyState(agent_name, agents, observation, done, time_step, episode_num=0
     # print(f'observation: {observation}, and Agent: {agent}')
     return agent.play_normal(observation, time_step)
 
-
-def create_env(num_of_agent, num_of_cycles, local_ratio, multiple, render_mode):
-    """
-    Creates the environment to be played in
-
-    num_of_agent = 3 - The number of agents to be created.  Default is default for Simple Spread
-
-    num_of_cycles = 25 - The number of frames (step for each agent).  Default is default for Simple Spread
-
-    local_ratio = 0.5 - Weight applied to local and global reward.  Local is collisions global is distance to landmarks.
-    Default is default for simple spread
-
-    multiple = False - Whether you need a parallel environment to be created
-    
-    render_mode = 'ansi' - Whether to be rendered on screen.  Default is not.
-    """
-    # continuous = False
-    if multiple:  # centralised must compute multiple in parallel
-        return parallel_env(N=num_of_agent, local_ratio=local_ratio, max_cycles=num_of_cycles, render_mode=render_mode)
-    return env(render_mode=render_mode, N=num_of_agent, max_cycles=num_of_cycles, local_ratio=local_ratio)
-
-
-# parallel_env(render_mode=render_mode, N=num_of_agent, max_cycles=num_of_cycles,
-#                           local_ratio=local_ratio)
-
-centralised = True
+multiple = True
 NUM_OF_AGENTS = 4
+NUM_OF_TIMESTEPS = 10
 
-env = create_env(NUM_OF_AGENTS, 10, 0.5, centralised, "human")
-
+if multiple:
+    env = parallel_env(render_mode="human", N=NUM_OF_AGENTS, max_cycles=NUM_OF_TIMESTEPS, local_ratio=0.5)
+else:
+    env = env(render_mode="human", N=NUM_OF_AGENTS, max_cycles=NUM_OF_TIMESTEPS, local_ratio=0.5)
 # env.reset()
 
 # env.render()
@@ -130,17 +106,12 @@ env = create_env(NUM_OF_AGENTS, 10, 0.5, centralised, "human")
 # plt.plot(steps, y)
 # plt.show()
 
-num_episode = int(input('Insert number of episodes: '))
-agent_type = AgentType.IQL
-agents = {f'agent_{i}': IndependentQLearning(f'agent_{i}', 10) for i in range(NUM_OF_AGENTS)}
+NUM_OF_EPISODES = int(input('Insert number of episodes: '))
+agents = {f'agent_{i}': IndependentQLearning(f'agent_{i}', NUM_OF_TIMESTEPS) for i in range(NUM_OF_AGENTS)}
 
-
-#print(agents)
-
-t = 0
 rewardX = []
 steps = []
-for i in range(0, num_episode):
+for i in range(0, NUM_OF_EPISODES):
 
     if (i % 50) == 0:
         print(i)
@@ -151,7 +122,7 @@ for i in range(0, num_episode):
     steps.append(i)
     t = 0
     rewardStep = []
-    while t < 10:
+    while t < NUM_OF_TIMESTEPS:
         # steps.append(t)
         t = t + 1
         actions = {}
@@ -190,11 +161,6 @@ for agent in agents:
     print(f'Final Qtable {to_dict(agents[agent].qTable)}')
 
 plt.plot(steps, rewardX)
-
-slope, intercept = np.polyfit(np.array(steps), np.array(rewardX), 1)
-trend_line = slope * np.array(steps) + intercept
-plt.plot(np.array(steps), trend_line, 'r-')
-
 plt.show()
 
 print("EVALUATING-------")
